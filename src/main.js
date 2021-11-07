754919921d45c3c5c806a8e7f6be94ca971fcee6
ycyoes <ycyoes@163.com>
2021-11-07 13:59:28 +0800

    import mavon-editor

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
+Vue.use(mavonEditor)
 
 Vue.config.productionTip = false
 
