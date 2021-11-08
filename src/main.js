035b7dc4f62d2ca87c623ab13d57fd06d34e9845
ycyoes <ycyoes@163.com>
2021-11-08 13:02:28 +0800

    package import

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import axios from 'axios'
+
 import mavonEditor from 'mavon-editor'
+
+import "element-ui/lib/theme-chalk/index.css"
 import 'mavon-editor/dist/css/index.css'
+
+import "./axios"
 import './permission.js' //路由拦截
 
 Vue.use(mavonEditor)
+Vue.use(Element)
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
